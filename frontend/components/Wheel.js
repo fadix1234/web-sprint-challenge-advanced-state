8c15e78eaea889220dbc3870eb8954de77098a6a
fadix1234 <ahmadzaiyasmin@yahoo.com>
2023-07-18 11:21:01 -0700

    done with wheel

--- frontend/components/Wheel.js
+++ frontend/components/Wheel.js
@@ -5,17 +5,20 @@ import { moveClockwise, moveCounterClockwise } from "../state/action-creators";
 //export default function Wheel(props) {
 
   const Counter = (props) => {
-   const { count, moveClockwise, moveCounterClockwise } = props;
-
+   const { wheel, moveClockwise, moveCounterClockwise } = props;
+   console.log(props);
   return (
     <div id="wrapper">
       <div id="wheel">
-        <div className="cog active" style={{ "--i": 0 }}>B</div>
-        <div className="cog" style={{ "--i": 1 }}></div>
-        <div className="cog" style={{ "--i": 2 }}></div>
-        <div className="cog" style={{ "--i": 3 }}></div>
-        <div className="cog" style={{ "--i": 4 }}></div>
-        <div className="cog" style={{ "--i": 5 }}></div>{/* --i is a custom CSS property, no need to touch that nor the style object */}
+        {
+          [...Array(6).keys()].map(key => {
+            return (
+              <div className={`cog${wheel === key ? ' active' : ''}`} key={key} style={{ "--i": key }}>
+                {wheel === key ? 'B' : null}
+              </div>
+            )
+          })
+        }{/* --i is a custom CSS property, no need to touch that nor the style object */}
       </div>
       <div id="keypad">
         <button id="counterClockwiseBtn" onClick={moveCounterClockwise} >Counter clockwise</button>
@@ -26,6 +29,6 @@ import { moveClockwise, moveCounterClockwise } from "../state/action-creators";
 }
 //}
 const mapStateToProps = (state) => {
-  return { count: state };
+  return { wheel: state.wheel };
 };
 export default connect(mapStateToProps, { moveClockwise, moveCounterClockwise})(Counter);
